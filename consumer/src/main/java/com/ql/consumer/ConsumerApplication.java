5766a2ab1a67f98b0c998ab966da1da29139e909
ql <1469981820@qq.com>
2021-05-16 11:55:30 +0800

    提供url连接对象的实例

--- consumer/src/main/java/com/ql/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/ql/consumer/ConsumerApplication.java
@@ -2,6 +2,8 @@ package com.ql.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumerApplication {
@@ -9,5 +11,9 @@ public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
-
+    //提供url的连接对象实例
+    @Bean
+    RestTemplate restTemplateOne(){
+        return new RestTemplate();
+    }
 }
