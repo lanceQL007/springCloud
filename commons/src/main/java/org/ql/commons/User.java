3b2df44a794f3496ad9f4bc90038a214092d8463
ql <1469981820@qq.com>
2021-05-18 21:53:23 +0800

    创建一个普通的 Maven 项目（为什么不创建springboot项目呢？因为springboot是可执行jre，不可以被依赖）作为 commons 模块，然后这个 commons 模块被 provider 和consumer 共同引用，这样我们就可以方便的传递 JSON 了。

---
+++ commons/src/main/java/org/ql/commons/User.java
@@ -0,0 +1,36 @@
+package org.ql.commons;
+
+/**
+ * @author LanceQ
+ * @version 1.0
+ * @time 2021/5/18 21:49
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
