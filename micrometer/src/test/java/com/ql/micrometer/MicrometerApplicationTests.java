fad94f504d3a4a9c3270f61bb85a6664798b2a1f
ql <1469981820@qq.com>
2021-05-30 19:26:04 +0800

    Micrometer监控模块

---
+++ micrometer/src/test/java/com/ql/micrometer/MicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ql.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
